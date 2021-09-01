928c13fca12cddc0d43895dd0113e614bffb78cf
AfianAvro <wahedemon09@gmail.com>
2021-09-01 23:26:46 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", () => {
+    const inputField = document.getElementById("inputField");
+    const inputFieldValue = inputField.value;
+    console.log(inputFieldValue)
+})
